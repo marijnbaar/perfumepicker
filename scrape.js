6ba2f261c3b3aa4dec10f8cfe370af71361081ec
Marijn Baar <marijn.baar@amsterdam.nl>
2025-02-22 16:15:04 +0100

    add new scrape script

--- scrape.js
+++ scrape.js
@@ -8,7 +8,7 @@ puppeteer.use(
 
 // Replace with the actual URL if it's different
 const SEARCH_URL =
-  "https://www.fragrantica.com/search";
+  "https://www.fragrantica.com/search/";
 const USER_AGENT =
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36";
 
